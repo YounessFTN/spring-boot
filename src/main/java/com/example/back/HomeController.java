9026d226156a6748622c2aa4bc198fef1fff774b
YounessFTN <youness.fatine1@gmail.com>
2026-02-04 11:46:31 +0100

    init

---
+++ src/main/java/com/example/back/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.back;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "forward:/index.html";
+    }
+}
